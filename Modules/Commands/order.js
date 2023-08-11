dc8817f9ec4d9fd88de1bf8f5a4d12e06ddc7676
Pokedi CLI <cli@pokedi.host>
2023-08-11 12:05:50 -0600

    ✨ [Order] created + better

---
+++ Modules/Commands/order.js
@@ -0,0 +1,41 @@
+import { SlashCommandBuilder } from "discord.js";
+
+export default {
+    help: "",
+    data: new SlashCommandBuilder()
+        .setName('order').setNameLocalizations({
+            "es-ES": "orden",
+            "pt-BR": "ordenar",
+            "de": "reihenfolge"
+        })
+        .setDescription('Select how you want to order your pokemon in the list')
+        .addStringOption(option => option.setName('style').setDescription('Select how you want your pokemon ordered').setRequired(true)
+            .addChoices({ name: 'Number', value: 'idx' },
+                { name: 'IV', value: 'iv' },
+                { name: 'Alphabetically', value: 'a' },
+                { name: 'Level', value: 'l' })
+        )
+        .addStringOption(option => option.setName("order").setDescription("Select whether you want the order to be ascending or descending").setRequired(true).addChoices({
+            name: "Ascending", value: "asc"
+        }, {
+            name: "Descending", value: "desc"
+        })),
+    async execute(msg) {
+        
+        let orderType = msg.options.getString("style"),
+            orderFlow = msg.options.getString("order");
+
+        await msg.client.prisma.users.update({
+            where: {
+                id: BigInt(msg.user.id)
+            },
+            data: {
+                order_by: `${orderType}-${orderFlow}`
+            }
+        });
+
+        await msg.reply("Order was updated!");
+
+    }
+}
+
