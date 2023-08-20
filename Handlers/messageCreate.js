2bda23f76b104c527d4c3c0a6aa33c0a14bf517e
PhoenixOnARoof <11891199+PhoenixOnARoof@users.noreply.github.com>
2023-08-20 15:31:40 -0600

    🤔 [messageEvent] User Level up crazy harder

--- Handlers/messageCreate.js
+++ Handlers/messageCreate.js
@@ -1,5 +1,7 @@
 import Chance from "chance";
 import Pokemon from "../Classes/pokemon.js";
+import Player from "../Classes/player.js";
+import randomint from "../Utilities/Misc/randomint.js";
 
 const chance = Chance();
 
@@ -25,6 +27,15 @@ async function messageCreate(msg, e) {
         // Send Message Test
         // msg.channel.send("Pokemon to Spawn Config: " + JSON.stringify(msg.channel.spawn.pokemon));
         await msg.channel.spawn.pokemon.spawnToChannel(msg);
+    }
+
+    if (!msg.author.player) msg.author.player = new Player(msg.author), await msg.author.player.fetch(msg.client.prisma);
+
+    if (msg.author.player && msg.author.player.started) {
+        if (randomint(10000) < 20) {
+            if (await msg.author.player.levelUp(msg.client.prisma))
+                msg.channel.send("Congrats on being Level " + msg.author.player.level + ", Champ.");
+        }
     }
 
     return true;
