1e0950fd43cab8eec766d9f2e97b92eba0eb3642
PhoenixOnARoof <11891199+PhoenixOnARoof@users.noreply.github.com>
2023-08-24 14:41:42 -0600

    🤔[Dev] new test env

--- test.js
+++ test.js
@@ -1,11 +1,3 @@
+import Player from "./Classes/player.js";
 import Pokemon from "./Classes/pokemon.js";
 import client from "./Services/Database/index.js";
-// import allPokemon from "./Utilities/Data/pokemon.js";
-
-const p = new Pokemon({});
-
-client.pokemon.findFirst({
-    where: {
-        user_id: BigInt()
-    }, orderBy: { idx: "desc" }
-})
