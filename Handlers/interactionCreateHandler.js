dd043bfbb54d8cb5b294a9a9c0f1d94212cc97be
PhoenixOnARoof <11891199+PhoenixOnARoof@users.noreply.github.com>
2023-08-31 02:57:59 -0600

    🧨 [interactionCreateHandler] channel & guild inits

--- Handlers/interactionCreateHandler.js
+++ Handlers/interactionCreateHandler.js
@@ -6,10 +6,30 @@ async function interactionCreateHandler(event) {
     if (!msg.guild || (msg.member && msg.member.user.bot))
         return;
 
-    // Init User
-    if (!msg.user.info) msg.user.info = await prisma.users.findUnique({ where: { id: BigInt(msg.user.id) } }) || {};
+    // Init Channel
+    if (!msg.channel.info) msg.channel.info = await msg.client.prisma.channels.findFirst({
+        where: {
+            id: BigInt(msg.channel.id)
+        }
+    }) || {}, msg.channel.configs = Object.assign(...(await msg.client.prisma.command_configuration.findMany({
+        where: {
+            channel_id: BigInt(msg.channel.id)
+        }
+    })).map(x => ({ [x.command]: x })));
 
-    if (msg.client.commands.get(msg.commandName)) return msg.client.commands.get(msg.commandName)(msg);
+    // Init Guild
+    if (!msg.guild.info) msg.guild.info = await msg.client.prisma.guilds.findFirst({
+        where: {
+            id: BigInt(msg.guild.id)
+        }
+    }) || {}, msg.guild.configs = Object.assign(...(await msg.client.prisma.command_configuration.findMany({
+        where: {
+            guild_id: BigInt(msg.guild.id)
+        }
+    })).map(x => ({ [x.command]: x })));
+
+    if (msg.client.commands.get(msg.commandName))
+        return msg.client.commands.get(msg.commandName)(msg);
 
     // msg.reply("Interaction Found, good job");
 }
