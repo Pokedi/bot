b481214875bf42d398d9870d5fced0fbb5cb135d
DittoDuckPenguin <moosaburn@gmail.com>
2023-11-15 01:21:18 -0700

    ✅ [Vote System] 12 hours => 24 hours

--- Services/Server/Routers/Votes/index.js
+++ Services/Server/Routers/Votes/index.js
@@ -42,7 +42,7 @@ voteRouter.post("/topgg", async (req, res) => {
             total_votes: 1,
             last_voted: new Date()
         })} ON CONFLICT (id, type) DO UPDATE SET 
-        streak = (CASE WHEN (extract(epoch from now()) - extract(epoch from user_vote.last_voted)) < (extract(epoch from interval '12 hours')) THEN (user_vote.streak + 1) ELSE 0 END),
+        streak = (CASE WHEN (extract(epoch from now()) - extract(epoch from user_vote.last_voted)) < (extract(epoch from interval '24 hours')) THEN (user_vote.streak + 1) ELSE 0 END),
         last_voted = EXCLUDED.last_voted, total_votes = user_vote.total_votes + 1`, x`INSERT INTO user_inventory ${sql({
             user_id: user,
             item_id: crate.id,
