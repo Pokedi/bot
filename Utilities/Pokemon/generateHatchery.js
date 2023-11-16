79b2b92570077c472687b61c1df29e9c0b53fedc
DittoDuckPenguin <moosaburn@gmail.com>
2023-11-17 00:49:56 +0330

    🤔 [Hatchery] finishing up image generation

--- Utilities/Pokemon/generateHatchery.js
+++ Utilities/Pokemon/generateHatchery.js
@@ -1,3 +1,37 @@
-export default async (user) => {
-    return Buffer.from("222")
+import sharp from "sharp";
+import Chance from "chance";
+export default async (hatchery = []) => {
+
+    const background = sharp('./EggHatchery.png');
+
+    const egg = sharp('./egg.png');
+
+    let nests = [];
+
+    if (hatchery.includes(1)) nests.push({
+        input: await egg.rotate(Chance().integer({ min: -20, max: 20 }), { background: [0, 0, 0, 0] }).toBuffer(),
+        top: 289,
+        left: 210
+    });
+
+    if (hatchery.includes(2)) nests.push({
+        input: await egg.rotate(Chance().integer({ min: -20, max: 20 }), { background: [0, 0, 0, 0] }).toBuffer(),
+        top: 289,
+        left: 627
+    });
+
+    if (hatchery.includes(3)) nests.push({
+        input: await egg.rotate(Chance().integer({ min: -20, max: 20 }), { background: [0, 0, 0, 0] }).toBuffer(),
+        top: 597,
+        left: 210
+    });
+
+    if (hatchery.includes(4)) nests.push({
+        input: await egg.rotate(Chance().integer({ min: -20, max: 20 }), { background: [0, 0, 0, 0] }).toBuffer(),
+        top: 597,
+        left: 617
+    });
+
+    return await background.composite(nests).png({ quality: 60, compressionLevel: 8 }).toBuffer();
+
 }
