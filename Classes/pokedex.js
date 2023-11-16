4fff817d3f4a4f8385d63d43bb0f69eb956da255
DittoDuckPenguin <moosaburn@gmail.com>
2023-11-16 01:49:29 -0700

    🧨[Pokedex] nerfing legendaries

--- Classes/pokedex.js
+++ Classes/pokedex.js
@@ -189,7 +189,7 @@ WHERE move_id in ${pokeapisql(this.pokedex.moves.filter(x => x.move_method == "m
         // Reject if cannot be generated
         if (!generatedPokemon) return false;
 
-        if (!forced && (generatedPokemon.is_nonspawnable || (generatedPokemon.is_legendary || generatedPokemon.is_sublegendary || generatedPokemon.is_mythical) && chance.d100() > 10)) {
+        if (!forced && (generatedPokemon.is_nonspawnable || (generatedPokemon.is_legendary || generatedPokemon.is_sublegendary || generatedPokemon.is_mythical) && chance.d100() >= 2)) {
             // Retry
             delete this.pokemon,
                 this.pokedex._id;
