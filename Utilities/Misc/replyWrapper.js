05de5a23bcda1c8d4655ed6cea364f60e76fe574
DittoDuckPenguin <moosaburn@gmail.com>
2025-06-11 00:16:27 +0330

    refactor: Simplify replyWrapper logic for handling message replies

--- Utilities/Misc/replyWrapper.js
+++ Utilities/Misc/replyWrapper.js
@@ -1,16 +1,19 @@
 export default function replyWrapper(msg, options) {
+
     if (msg.reply) {
+
         if (typeof options === "string") {
             return msg.reply(options);
         }
 
-        // Remove interaction-only properties if replying to a Message
-        if (msg.constructor.name === "Message") {
-            const safe = { content: options.content };
-            return msg.reply(safe);
-        }
-
-        // Safe for interactions
+        if (msg.constructor.name === "ChatInputCommandInteraction")
+            // Safe for interactions
         return msg.reply(options);
+        
+        // Remove interaction-only properties if replying to a Message
+        const safe = { content: options.content };
+        return msg.reply(safe);
+
     }
+    
 }
