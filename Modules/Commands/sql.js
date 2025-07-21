d17d1df835da02319a4e0938078a8a01e3037a8c
PhoenixOnARoof <11891199+PhoenixOnARoof@users.noreply.github.com>
2025-07-21 11:35:15 -0600

    🐛 fix: ensure interaction is deferred before executing SQL command

--- Modules/Commands/sql.js
+++ Modules/Commands/sql.js
@@ -12,6 +12,7 @@ export default {
     async execute(msg) {
         if (process.env.DEVIDS && JSON.parse(process.env.DEVIDS).includes(msg.user.id)) {
             try {
+                msg.deferReply();
                 const returnedData = msg.options.getBoolean("pokedb") ? await pokemondb.unsafe(msg.options.getString("command")) : await msg.client.postgres.unsafe(msg.options.getString("command"));
                 try {
                     await msg.reply("```JS\n" + textTable([].concat([returnedData.statement.columns.map(x => x.name)], returnedData.map(x => Object.values(x).map(x => x || ""))), { hsep: " | " }) + "\n```")
