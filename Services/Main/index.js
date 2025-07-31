2a47966f1b34517f70bc52b16f3a021d4cc2c99a
Anthony <11891199+PhoenixOnARoof@users.noreply.github.com>
2025-07-31 08:26:27 -0600

    🐛 fix: Add missing )

--- Services/Main/index.js
+++ Services/Main/index.js
@@ -73,7 +73,7 @@ client.login(process.env.TOKEN);
 // Graceful Exit
 process.on("SIGINT", () => {
     console.log(`[${new Date().toLocaleString()}][WATCHDOG] Process was exited!`)
-    client.destroy().then(() => { console.log(`[${new Date().toLocaleString()}][WATCHDOG] Client logged out!`});
+    client.destroy().then(() => { console.log(`[${new Date().toLocaleString()}][WATCHDOG] Client logged out!`) });
     process.exit();
 });
 
